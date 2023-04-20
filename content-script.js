8856473c8347f1675a91a5d9858f2fd3cadb3120
Jun <junwenzhang@outlook.com>
2023-04-20 17:52:26 -0400

    Completed js

---
+++ content-script.js
@@ -0,0 +1,40 @@
+const headers = document.querySelectorAll('h1, h2, h3, h4'); // list
+
+function createList(nodeList) {
+  const ul = document.createElement('ul');
+  ul.classList.add('toc-list')
+
+  nodeList.forEach(node => {
+    const li = document.createElement('li');
+    li.classList.add(node.tagName); // for indentation
+    li.innerText = node.innerText; // display
+    li.addEventListener('click', () => {
+      node.scrollIntoView()
+      node.classList.add('highlight');
+      setTimeout(() => node.classList.remove('highlight'), 1000);
+      document.querySelector('.toc-list').classList.toggle('hidden');
+      document.querySelector('.toc-button').classList.toggle('hidden');
+    });
+    ul.appendChild(li);
+  })
+
+  return ul;
+}
+
+const list = createList(headers);
+
+function createButton(ul) {
+  // initialized with the list hidden
+  const button = document.createElement('button');
+  button.classList.add('toc-button');
+  // click on it to open the list
+  button.addEventListener('click', () => ul.classList.toggle('hidden'));
+  // once you click on a list item, collapse the list
+  return button;
+}
+
+const button = createButton(list);
+
+
+document.querySelector('body').appendChild(button);
+document.querySelector('body').appendChild(list);
